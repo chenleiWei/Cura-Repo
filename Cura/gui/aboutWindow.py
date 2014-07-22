ae365aef7b3b9623478996f7fb8e69b989a4b158
Catrodigious <cat@errur.com>
2014-07-22 14:55:46 -0700

    Single printer selection; removed YOUMagine, abs; added to about page

--- Cura/gui/aboutWindow.py
+++ Cura/gui/aboutWindow.py
@@ -21,8 +21,9 @@ class aboutWindow(wx.Frame):
 		title.SetFont(wx.Font(18, wx.SWISS, wx.NORMAL, wx.BOLD))
 		s.Add(title, flag=wx.ALIGN_CENTRE|wx.EXPAND|wx.BOTTOM, border=5)
 
-		s.Add(wx.StaticText(p, -1, 'End solution for Open Source Fused Filament Fabrication 3D printing.'))
-		s.Add(wx.StaticText(p, -1, 'Cura is currently developed and maintained by Ultimaker.'))
+		s.Add(wx.StaticText(p, -1, 'This version of Cura has been optimized for the Series 1 by Type A Machines'))
+		s.Add(wx.StaticText(p, -1, 'Cura is a solution for Open Source Fused Filament Fabrication 3D printing.'))
+		s.Add(wx.StaticText(p, -1, 'Cura is currently developed and maintained by Daid and Ultimaker.'))
 
 		s.Add(wx.StaticText(p, -1, 'Cura is build with the following components:'), flag=wx.TOP, border=10)
 		self.addComponent('Cura', 'Graphical user interface', 'AGPLv3', 'https://github.com/daid/Cura')
