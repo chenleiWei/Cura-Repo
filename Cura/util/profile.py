2a987e32abe49bc396c1e41175e8d791455ae7d3
Catrodigious <cat@errur.com>
2014-07-26 23:40:57 -0700

    Changed start/end gcode filename to match all other start/end gcode modifications

--- Cura/util/profile.py
+++ Cura/util/profile.py
@@ -248,43 +248,8 @@ setting('object_center_x', -1, float, 'hidden', 'hidden')
 setting('object_center_y', -1, float, 'hidden', 'hidden')
 
 #######################################################################################
-#Type A 2013 Series 1 - Start
-setting("Type A Machines 2013 - Start.gcode", """;-- START GCODE --
-;Sliced at: {day} {date} {time}
-;Basic settings: Layer height: {layer_height} Walls: {wall_thickness} Fill: {fill_density}
-;Print time: {print_time}
-;Filament used: {filament_amount}m {filament_weight}g
-;Filament cost: {filament_cost}
-;M190 S{print_bed_temperature} ;Uncomment to add your own bed temperature line
-M106 S255
-G21        ;metric values
-G90        ;absolute positioning
-M106 S255    ;start with the fan on
-G28    ;move to endstops
-G1 X150 Y5  Z15.0 F{travel_speed} ;move the platform down 15mm
-M109 S{print_temperature} ;Uncomment to add your own temperature line
-G92 E0                  ;zero the extruded length
-G1 F200 E30              ;extrude 3mm of feed stock
-G92 E0                  ;zero the extruded length again
-G1 F{travel_speed}
-;Put printing message on LCD screen
-M117 Printing...
-""", str, 'alteration', 'alteration')
-#######################################################################################
-#Type A 2013 Series 1 - End
-setting("Type A Machines 2013 - End.gcode", """;-- END GCODE --
-M104 S0                     ;extruder heater off
-M140 S0                     ;heated bed heater off (if you have it)
-G91                                    ;relative positioning
-G1 E-1 F300                            ;retract the filament a bit before lifting the nozzle, to release some of the pressure
-G1 Z+0.5 E-5 X-20 Y-20 F{travel_speed} ;move Z up a bit and retract filament even more
-G28 X0 Y0                              ;move X/Y to min endstops, so the head is out of the way
-M84                         ;steppers off
-G90                         ;absolute positioning
-""", str, 'alteration', 'alteration')
-#######################################################################################
 #Type A 2014 Series 1 - Start
-setting("Type A Machines 2014 - Start.gcode", """;-- START GCODE --
+setting('TypeAStart.gcode', """;-- START GCODE --
 ;Sliced at: {day} {date} {time}
 ;Basic settings: Layer height: {layer_height} Walls: {wall_thickness} Fill: {fill_density}
 ;Print time: {print_time}
@@ -307,7 +272,7 @@ M117 Printing...
 """, str, 'alteration', 'alteration')
 #######################################################################################
 #Type A 2014 Series 1 - End
-setting("Type A Machines 2014 - End.gcode", """;-- END GCODE --
+setting('TypeAEnd.gcode', """;-- END GCODE --
 M104 S0     ;extruder heater off
 M140 S0     ;heated bed heater off (if you have it)
 G91         ;relative positioning
